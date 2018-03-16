c454a794a14f5da8a756effde5dc473b2320f1f3
igargov <iv.gargov@gmail.com>
2018-03-16 17:33:15 +0200

    Updated config to seed books

--- BookstoreApp.Data/Migrations/Configuration.cs
+++ BookstoreApp.Data/Migrations/Configuration.cs
@@ -1,8 +1,14 @@
 namespace BookstoreApp.Migrations
 {
+    using BookstoreApp.Data;
+    using BookstoreApp.Models;
+    using System;
+    using System.Collections.Generic;
     using System.Data.Entity.Migrations;
+    using System.IO;
+    using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<BookstoreApp.Data.BookstoreContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<BookstoreContext>
     {
         public Configuration()
         {
@@ -10,12 +16,75 @@ namespace BookstoreApp.Migrations
             AutomaticMigrationDataLossAllowed = true;
         }
 
-        protected override void Seed(BookstoreApp.Data.BookstoreContext context)
+        protected override void Seed(BookstoreContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            try
+            {
+                this.SeedBooks(context);
+            }
+            catch (Exception ex)
+            {
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data.
+            }
+        }
+
+        private void SeedBooks(BookstoreContext context)
+        {
+            var booksToSeed = ReadBooksFromFile();
+
+            foreach (var book in booksToSeed)
+            {
+                context.Books.AddOrUpdate(b => b.Isbn, book);
+            }
+
+            context.SaveChanges();
+        }
+
+        private List<Book> ReadBooksFromFile()
+        {
+            List<Book> books = new List<Book>();
+
+            using (StreamReader reader = new StreamReader(@"D:\Telelrik Academy\Team Projects\BookstoreApp\book30-listing-test.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    try
+                    {
+                        var line = reader.ReadLine().Split(',').ToArray();
+                        string author = line[4].Trim();
+                        string category = line[6].Trim();
+                        string isbn = line[0];
+                        string bookName = line[3];
+                        string url = line[2];
+
+                        var authorToAdd = new Author()
+                        {
+                            AuthorName = author
+                        };
+
+                        var categoryToAdd = new Category()
+                        {
+                            CategoryName = category
+                        };
+
+                        var bookToAdd = new Book()
+                        {
+                            BookName = bookName,
+                            Isbn = isbn,
+                            Author = authorToAdd,
+                            Category = categoryToAdd
+                        };
+
+                        books.Add(bookToAdd);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+
+            return books;
         }
     }
 }
