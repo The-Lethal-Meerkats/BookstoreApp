8dcf510ab065d3ae1a0619e881a185990f75121f
Nikolay Nikolov <nikolaytsnikolov@gmail.com>
2018-03-19 13:59:06 +0200

    Implementing ShoppingCartService

--- BookstoreApp.Services/Contracts/IShoppingCartService.cs
+++ BookstoreApp.Services/Contracts/IShoppingCartService.cs
@@ -1,16 +1,17 @@
 ﻿using BookstoreApp.Models;
 using System.Collections.Generic;
+using BookstoreApp.Services.ViewModels;
 
 namespace BookstoreApp.Services.Contracts
 {
     public interface IShoppingCartService
     {
-        int AddBookToShoppingCart();
+        int AddBookToShoppingCart(Book book, int userId);
 
-        int DeleteBookFromShoppingCart(int id);
+        int DeleteBookFromShoppingCart(Book book, int userId);
 
-        IList<ICollection<Book>> ShowUserShoppingCart(int userId);
+        IList<Book> ShowUserShoppingCart(int userId);
 
-        int PlaceOrderFromShoppingCart(int shoppingCartId);
+        int PlaceOrderFromShoppingCart(int userId, OrderModel orderModel);
     }
 }
--- BookstoreApp.Services/Implementation/ShoppingCartService.cs
+++ BookstoreApp.Services/Implementation/ShoppingCartService.cs
@@ -3,10 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BookstoreApp.Data;
+using BookstoreApp.Data.Contracts;
+using BookstoreApp.Models;
+using BookstoreApp.Services.Contracts;
+using BookstoreApp.Services.ViewModels;
 
 namespace BookstoreApp.Services.Implementation
 {
-    public class ShoppingCartService
+    public class ShoppingCartService : IShoppingCartService
     {
+        private readonly IUnitOfWork unitOfWork;
+
+        public ShoppingCartService(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public int AddBookToShoppingCart(Book book, int userId)
+        {
+
+            var shoppingCart = this.unitOfWork.ShoppingCarts.GetById(userId);
+
+            if (shoppingCart == null)
+            {
+                return -1;
+            }
+
+            shoppingCart.Books.Add(book);
+            return this.unitOfWork.SaveChanges();
+        }
+
+        public int DeleteBookFromShoppingCart(Book book, int userId)
+        {
+            var shoppingCart = this.unitOfWork.ShoppingCarts.GetById(userId);
+
+            if (shoppingCart == null)
+            {
+                return -1;
+            }
+
+            shoppingCart.Books.Remove(book);
+            return this.unitOfWork.SaveChanges();
+        }
+
+        public int PlaceOrderFromShoppingCart(int userId, OrderModel orderModel)
+        {
+            var shoppingCart = this.unitOfWork.ShoppingCarts.GetById(userId);
+
+            if (shoppingCart == null)
+            {
+                return -1;
+            }
+
+            var shoppingCartBooks = shoppingCart.Books.AsQueryable().ToList();
+
+            // TODO: - OrderModel to be implemented + CreateOrder Service method to be implemented? 
+
+            throw new NotImplementedException();
+        }
+
+        public IList<Book> ShowUserShoppingCart(int userId)
+        {
+            var shoppingCart = this.unitOfWork.ShoppingCarts.GetById(userId);
+
+            if (shoppingCart == null)
+            {
+                throw new ArgumentException();
+            }
+
+            var shoppingCartBooks = shoppingCart.Books.AsQueryable().ToList();
+
+            return shoppingCartBooks;
+        }
     }
 }
