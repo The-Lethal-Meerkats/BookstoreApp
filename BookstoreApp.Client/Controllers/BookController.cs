13fe7c82df7b54445b11b29dbd7b26e207d4d11f
igargov <iv.gargov@gmail.com>
2018-03-20 14:02:35 +0200

    Controllers directory added to Client with BookController class

---
+++ BookstoreApp.Client/Controllers/BookController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using BookstoreApp.Services.Contracts;
+using BookstoreApp.Services.ViewModels;
+using System;
+using System.Collections.Generic;
+
+namespace BookstoreApp.Client.Controllers
+{
+    public class BookController
+    {
+        private readonly IBookService bookService;
+        private readonly IMapper mapper;
+
+        public BookController(IBookService bookService, IMapper mapper)
+        {
+            this.bookService = bookService;
+            this.mapper = mapper;
+        }
+
+        public List<BookViewModel> GetAllBooks()
+        {
+            var storedBooks = this.bookService.GetAllBooks();
+
+            List<BookViewModel> booksModel = new List<BookViewModel>();
+
+            foreach (var book in storedBooks)
+            {
+                var mappedBook = this.mapper.Map<BookViewModel>(book);
+
+                booksModel.Add(mappedBook);
+            }
+
+            return booksModel;
+        }
+    }
+}
