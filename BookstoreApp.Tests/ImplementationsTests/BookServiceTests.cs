64d466cf4144ab6b378dee5174b2a9e6e5a81461
Sophia Kiryakova <sophia.kiryakova@gmail.com>
2018-03-25 23:34:37 +0300

    Added UnitTests project.

---
+++ BookstoreApp.Tests/ImplementationsTests/BookServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using AutoMapper;
+using BookstoreApp.Data.Contracts;
+using BookstoreApp.Services.Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace BookstoreApp.ImplementationsTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        // Should_ExpectedBehavior_When_StateUnderTest
+        [TestMethod]
+        public void Should_NotReturnNull_When_BookServiceCalled()
+        {
+            var mockUnitOfWork = new Mock<IUnitOfWork>();
+            var mockMapper = new Mock<IMapper>();
+
+            var bookService = new BookService(mockUnitOfWork.Object, mockMapper.Object);
+
+            Assert.IsNotNull(bookService);
+        }
+    }
+}
