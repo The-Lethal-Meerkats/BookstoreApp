8168d5250d522f85d567e68b4d3181e0469073fe
Sophia Kiryakova <sophia.kiryakova@gmail.com>
2018-03-30 17:14:30 +0300

    Added a bootstrap theme. Connected links to views. Added a picture for home page.

--- BookstoreApp.API/App_Start/BundleConfig.cs
+++ BookstoreApp.API/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace BookstoreApp.API
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/main.css"));
         }
     }
 }
