4bc30111db0a8360c7daf43e50eef5fc02a0df13
igargov <iv.gargov@gmail.com>
2018-03-31 17:48:22 +0300

    Autofac added for MVC. AutofacConfig mehtod added to build the container in Global.asax

---
+++ BookstoreApp.API/DI/AutofacConfig.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using BookstoreApp.Data.DI;
+using BookstoreApp.Services.DI;
+using System.Reflection;
+using System.Web.Mvc;
+
+namespace BookstoreApp.API.DI
+{
+    public class AutofacConfig
+    {
+        public static void ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+
+            //Register modules
+            builder.RegisterModule(new AutofacDataModule());
+            builder.RegisterModule(new AutofacServiceModule());
+
+            //Register MVC controllers
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
