abf45688767066665700b0b2a4a06b8c5f86d9c9
igargov <iv.gargov@gmail.com>
2018-04-01 02:55:52 +0300

    UserId claim added.

--- BookstoreApp.Data/Models/Accounts/BookstoreUser.cs
+++ BookstoreApp.Data/Models/Accounts/BookstoreUser.cs
@@ -19,6 +19,7 @@ namespace BookstoreApp.Models.Accounts
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
+            userIdentity.AddClaim(new Claim("UserId", this.Id.ToString(), "int32"));
             return userIdentity;
         }
     }
