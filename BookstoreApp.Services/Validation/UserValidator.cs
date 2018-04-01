c90a535c1df1d2ad5536a66826be4c7e76bac253
Nikolay Nikolov <nikolaytsnikolov@gmail.com>
2018-04-01 13:06:04 +0300

    Adding Author,BookValidator

--- BookstoreApp.Services/Validation/UserValidator.cs
+++ BookstoreApp.Services/Validation/UserValidator.cs
@@ -4,18 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BookstoreApp.Models;
+using BookstoreApp.Models.Accounts;
 using FluentValidation;
 
 namespace BookstoreApp.Services.Validation
 {
-    class UserValidator : AbstractValidator<User>
+    class UserValidator : AbstractValidator<BookstoreUser>
     {
         public UserValidator()
         {
             RuleFor(user => user.FirstName).NotEmpty();
             RuleFor(user => user.LastName).NotEmpty();
             RuleFor(user => user.Email).EmailAddress();
-            RuleFor(user => user.Password).NotEmpty();
             RuleFor(user => user.PhoneNumber).NotEmpty();
             RuleFor(user => user.UserAddress).NotEmpty();
         }
