f63f8ca5d705a6bd02e2e59f7aa9fe1e0b33a20c
Nikolay Nikolov <nikolaytsnikolov@gmail.com>
2018-04-01 14:18:10 +0300

    WishlistController implemented

--- BookstoreApp.API/Controllers/WishlistController.cs
+++ BookstoreApp.API/Controllers/WishlistController.cs
@@ -3,14 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BookstoreApp.Services.Contracts;
+using BookstoreApp.Services.Implementation;
+using Microsoft.Owin.Security;
 
 namespace BookstoreApp.API.Controllers
 {
     public class WishlistController : Controller
     {
-        // GET: Wishlist
-        public ActionResult Index()
+        private IWishlistService wishlistService;
+        private IAuthenticationManager authenticationManager;
+        public WishlistController(IWishlistService wishlistService, IAuthenticationManager authenticationManager)
         {
+            this.wishlistService = wishlistService ?? throw new ArgumentNullException();
+            this.authenticationManager = authenticationManager ?? throw new ArgumentNullException();
+        }
+        // GET: Wishlist
+        public ActionResult WishlistBookCollection()
+        {
+            var user = this.authenticationManager.User.FindFirst(c => c.Type == "UserId");
+            var userId = int.Parse(user.Value);
+            var books = wishlistService.GetUserWishlistBooks(userId);
+            return View(books);
+        }
+
+        public ActionResult AddToWishlist(int bookId)
+        {
+            
+            var user = this.authenticationManager.User.FindFirst(c => c.Type == "UserId");
+            var userId = int.Parse(user.Value);
+            try
+            {
+                wishlistService.AddBookToWishlist(bookId, userId);
+            }
+            catch (Exception ex)
+            {
+                //TODO : Add unsuccessful result
+                return View();
+            }
+
+            //TODO: Add success view 
+            return View();
+        }
+
+        public ActionResult DeleteFromWishlist(int bookId)
+        {
+            var user = this.authenticationManager.User.FindFirst(c => c.Type == "UserId");
+            var userId = int.Parse(user.Value);
+            try
+            {
+                wishlistService.DeleteBookFromWishlist(bookId, userId);
+            }
+            catch (Exception ex)
+            {
+                //TODO : Add unsuccessful result
+                return View();
+            }
+
+            //TODO: Add success view 
             return View();
         }
     }
