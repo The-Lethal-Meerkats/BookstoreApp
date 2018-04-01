8905a894f0d7bc6c4d4b435b09a05cbc80415ef4
Nikolay Nikolov <nikolaytsnikolov@gmail.com>
2018-04-01 12:40:34 +0300

    Adding FluentValidation and Validation for User Module

---
+++ BookstoreApp.Services/Validation/UserValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookstoreApp.Models;
+using FluentValidation;
+
+namespace BookstoreApp.Services.Validation
+{
+    class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(user => user.FirstName).NotEmpty();
+            RuleFor(user => user.LastName).NotEmpty();
+            RuleFor(user => user.Email).EmailAddress();
+            RuleFor(user => user.Password).NotEmpty();
+            RuleFor(user => user.PhoneNumber).NotEmpty();
+            RuleFor(user => user.UserAddress).NotEmpty();
+        }
+    }
+}
