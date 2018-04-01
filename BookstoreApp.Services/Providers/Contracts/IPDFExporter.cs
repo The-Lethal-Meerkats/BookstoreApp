d906b2c323211eaa85ee5029a56b02ec7f9f2e01
Nikolay Nikolov <nikolaytsnikolov@gmail.com>
2018-04-02 02:51:05 +0300

    PDFExporter implementation

---
+++ BookstoreApp.Services/Providers/Contracts/IPDFExporter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookstoreApp.Services.ViewModels;
+
+namespace BookstoreApp.Services.Providers.Contracts
+{
+    interface IPDFExporter
+    {
+        void ExportOrders(IEnumerable<OrderViewModel> orders);
+    }
+}
